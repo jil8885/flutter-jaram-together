0b0cab8bd97a2ad6f9cd12733e8f84b56294d317
krumaska <okokrt@gmail.com>
2020-08-10 21:54:02 +0900

    Create styles.dart

---
+++ lib/style/styles.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+const MaterialColor mdarkBlue = const MaterialColor(
+  0xFFFFFFFF,
+  const <int, Color>{
+    50: const Color(0xff61a0af),
+    100: const Color(0xff61a0af),
+    200: const Color(0xff61a0af),
+    300: const Color(0xff61a0af),
+    400: const Color(0xff61a0af),
+    500: const Color(0xff61a0af),
+    600: const Color(0xff61a0af),
+    700: const Color(0xff61a0af),
+    800: const Color(0xff61a0af),
+    900: const Color(0xff61a0af),
+  },
+);
+
+const kdarkBlueColor = Color(0xff61a0af);
+const kbannaColor = Color(0xfff5d491);
+
+BoxShadow wBoxshadow = BoxShadow(
+  color: Color(0xFF1B1D1F).withOpacity(0.1),
+  spreadRadius: 0,
+  blurRadius: 1,
+  offset: Offset(0, 2), // changes position of shadow
+);
