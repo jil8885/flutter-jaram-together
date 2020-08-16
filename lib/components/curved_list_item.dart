e0aa144ee3364b1db9f3bfbb5e69d3c00ef326af
krumaska <okokrt@gmail.com>
2020-08-17 00:58:33 +0900

    Create curved_list_item.dart round design

---
+++ lib/components/curved_list_item.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+import '../style/styles.dart';
+import '../style/styles.dart';
+
+class CurvedListItem extends StatelessWidget {
+  const CurvedListItem({
+    this.title,
+    this.time,
+    this.icon,
+    this.people,
+    this.color,
+    this.nextColor,
+    this.child,
+  });
+
+  final String title;
+  final String time;
+  final String people;
+  final IconData icon;
+  final Color color;
+  final Color nextColor;
+  final Widget child;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: nextColor,
+      child: Container(
+        decoration: BoxDecoration(
+          color: color,
+          borderRadius: const BorderRadius.only(
+            bottomLeft: Radius.circular(80.0),
+          ),
+          boxShadow: [wBoxshadow],
+        ),
+        padding: const EdgeInsets.only(
+          left: 32,
+          top: 80.0,
+          bottom: 50,
+        ),
+        child: child,
+      ),
+    );
+  }
+}
